2ae94182aa857fc0a003b96004881aa02a78e95c
Xamanek Mtz M - Blackberry <xamanekmtz@gmail.com>
2023-08-07 12:05:10 -0600

    Aplicada la configuración externa via archivos YML que utilizan diferentes parametros en estos archivos para generar diferentes experimentos

--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-from src.dm import *
-from src.module import *
+# from src.dm import *
+# from src.module import *
+from src import *
 from pathlib import Path
 import pytorch_lightning as pl
+import yaml
+import sys
 
-path = Path('dataset')
-dm = MNISTDataModule( path, batch_size = 25 )
-module = MNISTModule()
-trainer = pl.Trainer(
-    max_epochs = 10, 
-    logger = None, 
-    enable_checkpointing = False, 
-    # overfit_batches = 1,
-)
+# path = Path('dataset')
+# dm = MNISTDataModule( path, batch_size = 25 )
+# module = MNISTModule()
+# trainer = pl.Trainer(
+#     max_epochs = 10, 
+#     logger = None, 
+#     enable_checkpointing = False, 
+#     # overfit_batches = 1,
+# )
 
-trainer.fit ( module, dm )
+# trainer.fit ( module, dm )
+
+# config = {
+#     'datamodule': {
+#         'path': Path('dataset'), 
+#         'batch_size': 25, 
+#     }, 
+#     'trainer': {
+#         'max_epochs': 10, 
+#         'logger': None, 
+#         'enable_checkpointing': False, 
+#         'overfit_batches': 0, 
+#     },
+# }
+
+# dm = MNISTDataModule( **config['datamodule'] )
+# module = MNISTModule()
+# trainer = pl.Trainer( **config['trainer'] )
+# trainer.fit( module, dm )
+
+config = {
+    'datamodule': {
+        'path': Path('dataset'), 
+        'batch_size': 25, 
+    },
+    'trainer': {
+        'max_epochs': 10, 
+        'logger': None, 
+        'enable_checkpointing': False,
+        'overfit_batches': 0,
+    },
+}
+
+def train(config):
+    dm = MNISTDataModule( **config['datamodule'] )
+    module = MNISTModule()
+    trainer = pl.Trainer( **config['trainer'] )
+    trainer.fit( module, dm )
+
+if __name__ == '__main__':
+    if len( sys.argv ) > 1:
+        config_file = sys.argv[1]
+        if config_file:
+            with open( config_file, 'r' ) as stream:
+                loaded_config = yaml.safe_load( stream )
+            deep_update( config, loaded_config )
+    print( config )
+    train( config )
